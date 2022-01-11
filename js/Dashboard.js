22f0e96eb7d05d07f2941dc1fc3eebb775c54a11
Ennyolar96 <ennyolar1996@gmail.com>
2022-01-11 16:38:16 +0100

    Add Javascript code and change text on index page

---
+++ js/Dashboard.js
@@ -0,0 +1,9 @@
+var table = document.getElementById("Opp_table");
+
+var newElement = document.createElement("td");
+var textNode = document.createTextNode("name");
+var textNode1 = document.createTextNode("name1");
+var textNode2 = document.createTextNode("name2");
+
+newElement.appendChild(textNode);
+table.appendChild(newElement);
