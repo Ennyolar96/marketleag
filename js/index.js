5edc27d45ce8d8a0e9fd36571b03a357dea71116
Ennyolar96 <ennyolar1996@gmail.com>
2022-01-30 11:48:15 +0100

    Edit

--- js/index.js
+++ js/index.js
@@ -18,6 +18,15 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "header") {
+    x.className += " responsive";
+  } else {
+    x.className = "header";
+  }
+}
 
 // set and getting the cookies for the user
 
