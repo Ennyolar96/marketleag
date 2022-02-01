151917aa160a80bcc6be938bdabd82108828cd2f
Ennyolar96 <ennyolar1996@gmail.com>
2022-02-01 14:07:37 +0100

    change

--- Dashboard/inventor/newinventor.js
+++ Dashboard/inventor/newinventor.js
@@ -86,6 +86,34 @@ const validateInputs = () => {
         setSuccess(email);
     }
 
+
+    const showMenu = (toggleId, navbarId,bodyId) =>{
+        const toggle = document.getElementById(toggleId),
+        navbar = document.getElementById(navbarId),
+        bodypadding = document.getElementById(bodyId)
+    
+        if(toggle && navbar){
+            toggle.addEventListener('click', ()=>{
+                // APARECER MENU
+                navbar.classList.toggle('show')
+                // ROTATE TOGGLE
+                toggle.classList.toggle('rotate')
+                // PADDING BODY
+                bodypadding.classList.toggle('expander')
+            })
+        }
+    }
+    showMenu('nav-toggle','navbar','body')
+    
+    // LINK ACTIVE COLOR
+    const linkColor = document.querySelectorAll('.nav__link');   
+    function colorLink(){
+        linkColor.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    }
+    
+    linkColor.forEach(l => l.addEventListener('click', colorLink));
+
     // New Ip Expert validation
 
 
