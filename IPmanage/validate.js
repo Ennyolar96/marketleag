b9d687e1951ecc9df2c42f59b1d110a3e6ae50c2
Ennyolar96 <ennyolar1996@gmail.com>
2022-03-10 16:36:29 +0100

    added

---
+++ IPmanage/validate.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("regForm");
+const myInput = document.getElementById("psw");
+const letter = document.getElementById("letter");
+const capital = document.getElementById("capital");
+const number = document.getElementById("number");
+const length = document.getElementById("length");
